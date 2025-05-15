ccbea6dc6f9d16abc690f63e82fd24269614648e
shivy2725 <shivamsharma2792004@gmail.com>
2025-05-15 08:45:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";// treat all js code as being strict 
+
+//alert("hello world")
+
+//alert(3 + 3 ) // we are using nodejs not brower 
+
+
+let name = "shivam" //string datatype
+let age = 20//number datatype 2 to power 53 
+let istrue = true //boolean datatype 
+let experiance = null // null datatype 
+let state ;
+ 
+//null type is an object 
+//symol = unique identifier
+//object = collection of key value pair 
+
+console.table([name,age,istrue,experiance,state])
+console.log(typeof name , typeof age, typeof istrue , typeof experiance, typeof state) 
