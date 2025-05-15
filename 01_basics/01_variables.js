0b0f748499c052cc24d0647cf0e24207f73e54ff
shivy2725 <shivamsharma2792004@gmail.com>
2025-05-15 08:51:29 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const account_Id = 144553 
+let accountemail = "shivam@google.com"
+var accountpassward = "12345"
+accountcity = "bhilai"
+
+//account_Id = 2 
+accountemail = "sjkadd@.com"
+accountpassward = "123"
+accountcity = "delhi"
+let accountstate ;
+
+// mostly used let variable
+//const account_Id = 2 // cannot be re-assigned
+console.table([account_Id, accountemail, accountpassward, accountcity,accountstate])
+
+console.log(account_Id);
