b905535fc0c0a11d0fdd48c936bb1df4f6b0334e
shivy2725 <shivamsharma2792004@gmail.com>
2025-05-20 06:51:41 +0000

    save

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -18,7 +18,7 @@ const numberarray = [1,2,3,[4,5,6],7,[4,5,6,[1,3]]]
 const newarr = numberarray.flat(Infinity)
 console.log(newarr);
 
-console.log(Array.isArray("shivam"));//for checking is array or not 
+console.log(Array.isArray("shivam"));//for checking is in array or not 
 console.log(Array.from("shivam"));// changing string to array 
 
 console.log(Array.from({name:"shivam"}));//iteresting case
