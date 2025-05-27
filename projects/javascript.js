0c090352c06b51e4683639a36834995b7b7058ee
shivy2725 <shivamsharma2792004@gmail.com>
2025-05-27 08:49:20 +0000

    save

---
+++ projects/javascript.js
@@ -0,0 +1,30 @@
+const button = document.querySelectorAll('.box');
+const body = document.querySelector('body');
+const reset = document.querySelector('#reset');
+
+button.forEach(function(btn){
+    btn.addEventListener('click', function(color){
+        if(color.target.id === 'box1'){
+            body.style.backgroundColor = 'aqua'
+        }
+        else if(color.target.id === 'box2'){
+            body.style.backgroundColor = 'gold'
+        }
+        else if(color.target.id === 'box3'){
+            body.style.backgroundColor = 'red'
+        }
+        else if(color.target.id === 'box4'){
+            body.style.backgroundColor = 'greenyellow'
+        }
+        else if(color.target.id === 'box5'){
+            body.style.backgroundColor = 'violet'
+        }
+
+       
+        
+    })
+
+})
+
+
+
